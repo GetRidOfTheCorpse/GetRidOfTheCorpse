e2cabbd8fa786d23a45292ea45118dde3e3e12e0
Daniel <daniel.fedh@gmail.com>
2015-01-24 14:50:23 +0100

    duard path and detection implementet

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera Camera;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.transform.position = this.transform.position;
+        Camera.transform.position += new Vector3(0, 0, -10);
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.position += Vector3.up * Input.GetAxis("Vertical");
+        this.transform.position += Vector3.right * Input.GetAxis("Horizontal");
+
+        Camera.transform.position = Vector3.Lerp(Camera.transform.position, this.transform.position, 0.001f);
+
+    }
+}
