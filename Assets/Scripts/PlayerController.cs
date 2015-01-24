7e8b6c23b99624f84487b9a1d0b126923e20a28d
Markus Brenner <markus.brenner@uni-ulm.de>
2015-01-24 02:22:51 +0100

    Body carrying works

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,33 +4,23 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	private GameObject body;
 	private Transform myTransform;
-	private Transform bodyTransform;
 	private float rot;
+	public float pickupdist = 0.5f;
 
-	private bool coll = false;
-	private bool pickup = false;
 	private Vector2 movDirection;
 
 	private int xm = 0;
 	private int ym = 0;
 
-	public bool hasBody(){
-		return pickup;
-	}
-
 	// Use this for initialization
 	void Start () {
-		GameObject[] objs = GameObject.FindGameObjectsWithTag("Body");
-		body = objs[0];	
-		myTransform = ((Transform)GetComponent("Transform"));
-		bodyTransform = ((Transform)body.GetComponent("Transform"));
+		myTransform = (Transform) GetComponent("Transform");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Rigidbody2D rigidBody = (Rigidbody2D)GetComponent("Rigidbody2D");
 		
-
 		if(Input.GetKey("up")){
 			ym = 1;
 		}
@@ -54,37 +44,35 @@ public class PlayerController : MonoBehaviour {
 		if(xm != 0 || ym != 0) rot = Mathf.Atan2(ym, xm)*Mathf.Rad2Deg;
 
 
-		if(Input.GetKeyDown("space") && coll){
-			pickup = true;
-			((SpriteRenderer)body.GetComponent("SpriteRenderer")).enabled = false;
+		if(Input.GetKeyDown("space") && body == null){
+			GameObject[] obj = GameObject.FindGameObjectsWithTag("Body");
+			for(int i = 0; i < obj.Length; i++){
+				Transform other = ((Transform) obj[i].GetComponent("Transform"));
+				float magn = (other.position - myTransform.position).magnitude;
+				if(magn < pickupdist){
+					body = obj[i];
+					DistanceJoint2D joint = gameObject.AddComponent<DistanceJoint2D>();
+					joint.distance = magn;
+					joint.connectedBody = (Rigidbody2D)body.GetComponent("Rigidbody2D");
+					joint.maxDistanceOnly = true;
+					break;
+				}
+			}
 		}
-		else if(Input.GetKeyUp("space")){
-			pickup = false;
-			((SpriteRenderer)body.GetComponent("SpriteRenderer")).enabled = true;
+		else if(Input.GetKeyUp("space") && body != null){
+			GameObject.Destroy(gameObject.GetComponent("DistanceJoint2D"));
+			body = null;
 		}
 
 		movDirection = new Vector2(xm, ym);
 		myTransform.rotation = Quaternion.Euler(0, 0, rot);
-		if(pickup)
-			movDirection *= 2;
-		else
-			movDirection *= 5;
+		movDirection *= 5;
+
+		if(body != null){
+			Transform other = (Transform) body.GetComponent("Transform");
+			other.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(other.position.y - myTransform.position.y, other.position.x - myTransform.position.x) * Mathf.Rad2Deg);
+		}
 
 		rigidBody.velocity = movDirection;
-		if(pickup){
-			bodyTransform.position = myTransform.position;
-		}
-	}
-
-	void OnTriggerEnter2D(Collider2D other){
-		if(other.tag == "Body"){
-			coll = true;
-		}
-	}
-
-	void OnTriggerExit2D(Collider2D other){
-		if(other.tag == "Body"){
-			coll = false;
-		}
 	}
 }
