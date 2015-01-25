715eae2ca70a163e1d6a00897765c03d72236cb6
Daniel <daniel.fedh@gmail.com>
2015-01-25 09:33:02 +0100

    added main menu

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour
+{
+
+    public string nextLevel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Application.LoadLevel(nextLevel);
+
+        }
+    }
+}
